d0ff330efb5be477f0166977d1475301fb631b54
looneym <looneymicheal@gmail.com>
2017-08-22 23:25:27 +0100

    Move nav to it's own component

--- project/static/scripts/jsx/main.js
+++ project/static/scripts/jsx/main.js
@@ -1,56 +1,9 @@
-var axios = require('axios');
-var react = require('react');
+import axios from 'axios';
+import React, { Component }  from 'react';
+
+import Nav from './components/nav';
 
 
-class Nav extends react.Component {  
-
-  constructor(props){
-    super(props);
-    this.testGet();
-  };
-
-  render(){
-    return (
-      <nav id="mainNav" className="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div className="container">
-          <div className="navbar-header page-scroll">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars" />
-            </button>
-            <a className="navbar-brand" href="/">Imgrab</a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li className="hidden">
-                <a href="#page-top" />
-              </li>
-              <li className="page-scroll">
-                <a href="/images">My Images</a>
-              </li>
-              <li className="page-scroll">
-                <a href="#">Help</a>
-              </li>
-              <li className="page-scroll">
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  };
-
-  testGet() {
-    axios.get('http://127.0.0.1:5000/api/images')
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-};
 
 
 
